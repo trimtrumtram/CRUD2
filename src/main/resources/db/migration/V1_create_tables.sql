2a2f1553b447ba92160ab18d05a09ea1f4023eda
trimtrumtram <s.rebnev@mail.ru>
2025-06-25 16:11:07 +0300

    commit message

--- src/main/resources/db/migration/V1_create_tables.sql
+++ src/main/resources/db/migration/V1_create_tables.sql
@@ -35,5 +35,12 @@ CREATE TABLE product (
                          price NUMERIC(10, 2) NOT NULL
 );
 
+CREATE TABLE order_products (
+    order_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
 
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
+);
 
